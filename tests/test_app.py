2c84611d7c782fd7dcba72319d6d18457593dd10
Nit-thin <itxnithin@gmail.com>
2026-03-31 12:54:39 +0530

    Create test_app.py

---
+++ tests/test_app.py
@@ -0,0 +1,18 @@
+import pytest
+from app import app
+
+
+@pytest.fixture
+def client():
+    app.config['TESTING'] = True
+    with app.test_client() as client:
+        yield client
+
+
+def test_home_page(client):
+    response = client.get("/")
+    assert response.status_code == 200
+
+
+def test_app_running():
+    assert app is not None
